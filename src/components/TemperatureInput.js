2f891d80a9e10c0158a73254f3adf67e5ae868a7
Naima Hassan <naima.hassan434@gmail.com>
2021-09-13 17:16:05 +0100

    Temperature Input: convert class to functional

--- src/components/TemperatureInput.js
+++ src/components/TemperatureInput.js
@@ -4,26 +4,14 @@ const scaleName = {
   c: "Celsius",
   f: "Fahrenheit",
 };
-class TemperatureInput extends React.Component {
-  state = {
-    temperature: "",
-  };
-
-  onTempChange = (e) => {
-    this.setState({ temperature: e.target.value });
-  };
-
-  render() {
-    const { temperature } = this.state;
-    const { scale } = this.props;
-    return (
-      <div>
-        <fieldset>
-          <legend>Enter temperature in {scaleName[scale]}</legend>
-          <input type="text" value={temperature} onChange={this.onTempChange} />
-        </fieldset>
-      </div>
-    );
-  }
+function TemperatureInput({ temperature, scale, onTemperatureChange }) {
+  return (
+    <div>
+      <fieldset>
+        <legend>Enter temperature in {scaleName[scale]}</legend>
+        <input type="text" value={temperature} onChange={onTemperatureChange} />
+      </fieldset>
+    </div>
+  );
 }
 export default TemperatureInput;
